5368ef000480df78a27eb77cffee7f4ab3e355e3
Valens NIYONKURU <int0000104@revenue.local>
2025-05-23 11:33:24 +0200

    springboot

---
+++ src/main/java/com/springboot_for_beginner/SpringbootForBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_for_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootForBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootForBeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_for_beginner/SpringbootForBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_for_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootForBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
