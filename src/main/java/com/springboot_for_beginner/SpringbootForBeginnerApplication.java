ffc37c77117fdd3324e736d05f245ce1ea963b37
Valens NIYONKURU <int0000104@revenue.local>
2025-05-25 07:24:06 +0200

    helloword

--- src/main/java/com/springboot_for_beginner/SpringbootForBeginnerApplication.java
+++ src/main/java/com/springboot_for_beginner/SpringbootForBeginnerApplication.java
@@ -2,12 +2,20 @@ package com.springboot_for_beginner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootForBeginnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootForBeginnerApplication.class, args);
+	}
+
+	@GetMapping("/greet")
+	public String greeting() {
+		return "Hello Valens";
 	}
 
 }
